24826f66cbcb8c67d2eb55f4235d29c789bdef55
Gregory Testo <gt@gregorytesto.com>
2021-10-21 16:28:51 -0400

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Hellos World</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+import * as React from 'react';
+import { Button, Text, ScrollView } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const HomeScreen = ({ navigation }) => {
+    let [items, setItems] = React.useState([]);
+    React.useEffect(()=>{
+        fetch("https://jsonplaceholder.typicode.com/todos")
+            .then((res)=>res.json())
+            .then((data)=>setItems(data));
+    }, []);
+      let itemsToDisplay = items.map((item)=>{
+        return <Text key={item.id}>{item.title}</Text>
+      })
+
+    return (
+        <ScrollView>
+            {itemsToDisplay}
+        </ScrollView>
+    //   <Button
+    //     title="Go to Jane's profile"
+    //     onPress={() =>
+    //       navigation.navigate('Profile', { name: 'Jane' })
+    //     }
+    //   />
+    );
+};
+  
+const ProfileScreen = ({ navigation, route }) => {
+    return <Text>This is {route.params.name}'s profile</Text>;
+};
+
+export default function App(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+        //   options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
